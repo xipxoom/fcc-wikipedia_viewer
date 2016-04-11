2c22443ee742d0cf6e5a602d3da864fac763d352
George Dhoore <georgiepoo@gmail.com>
2016-04-10 20:10:50 -0400

    Search as you type

--- site/js/app.js
+++ site/js/app.js
@@ -3,31 +3,37 @@ $(function(){
 
   $('#searchButton').on('click', function(){
     if ($('#searchText').val()) {
-      $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php' +
-             '?action=opensearch' +
-             '&search=' + $('#searchText').val() +
-             '&format=json' +
-             '&callback=?',
-        dataType: 'json',
-        headers: {
-          'Api-User-Agent': 'DhooreFCCWikisearch'
-        }
-      })
-      .done(function(json){
-        displaySearchResults(json);
-      });
+      retrieveSearchResults();
     } else {
       window.open('http://en.wikipedia.org/wiki/Special:Random', '_blank');
     }
   });
 
+  $('#searchText').on('keyup', function(keyPress){
+    retrieveSearchResults();
+  });
+
 });
 
+function retrieveSearchResults() {
+  $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php' +
+         '?action=opensearch' +
+         '&search=' + $('#searchText').val() +
+         '&format=json' +
+         '&callback=?',
+    dataType: 'json',
+    headers: {
+      'Api-User-Agent': 'DhooreFCCWikisearch'
+    }
+  })
+  .done(function(json){
+    displaySearchResults(json);
+  });
+}
 
 function displaySearchResults(results){
-  console.log(results);
-
+  $('main ul').html('');
   for (var i = 0, len = results[1].length; i < len; i++) {
     $("main ul").loadTemplate("template-searchResult.html", {
       link: results[3][i],
